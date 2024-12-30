45f14e84cb9db46b4b44a83182c24665ba598779
Anton Scheitler <anton.scheitler@gmail.com>
2024-12-30 12:17:21 +0100

    done

--- src/main/kotlin/com/example/highlight/HighlightClickableUIAction.kt
+++ src/main/kotlin/com/example/highlight/HighlightClickableUIAction.kt
@@ -7,10 +7,6 @@ import java.awt.Color
 import java.awt.Component
 import java.awt.Container
 import java.awt.Window
-import javax.swing.JComboBox
-import javax.swing.JList
-import javax.swing.JMenuItem
-import javax.swing.JTextField
 import javax.swing.SwingUtilities
 
 class HighlightClickableUIAction : AnAction() {
@@ -18,22 +14,27 @@ class HighlightClickableUIAction : AnAction() {
         val originalColors = HashMap<Component, Color>()
         var isHighlighted = false
 
+        /**
+         * recursively colors all containers in the given component
+         *
+         * @param component the component to iterate through
+         * @param color the background color to highlight with
+         */
         fun colorAllElements(component: Component, color: Color) {
             if (!component.isVisible) return
 
             if (component is Container) {
                 val children = component.components
+                // iterate through all children recursively
                 for (child in children) {
                     colorAllElements(child, color)
                 }
+                // color container, if it has not been colored already
                 if (!originalColors.contains(component)) {
-                    if (children.isEmpty() || component is JTextField || component is JComboBox<*> ||
-                        component is JList<*> || component is JMenuItem
-                    ) {
-                        originalColors[component] = component.background
-                        component.background = color
-                        component.repaint()
-                    }
+                    // store its original color
+                    originalColors[component] = component.background
+                    component.background = color
+                    component.repaint()
                 }
             }
         }
@@ -42,12 +43,15 @@ class HighlightClickableUIAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val windows = Window.getWindows()
         SwingUtilities.invokeLater {
+            // toggle highlighting on or off
             if (isHighlighted) {
+                // reset background colors
                 originalColors.forEach{ (component, color) ->
                     component.background = color
                 }
                 originalColors.clear()
             } else {
+                // highlight all elements in every window
                 for (window in windows) {
                     if (window.isVisible) {
                         colorAllElements(window, JBColor.YELLOW)
